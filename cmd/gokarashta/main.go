af9b96e7cec12103581b47aec875d830b1796d57
Kevin Thompson <phlux@ewnix.net>
2023-08-14 18:06:52 -0500

    Rewrite. Let's not bring in ImageMagick...

--- cmd/gokarashta/main.go
+++ cmd/gokarashta/main.go
@@ -2,156 +2,147 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"image"
+	"image/png"
+	"log"
 	"net/http"
 	"os"
-	"context"
-	"os/exec"
 
+	"github.com/go-ldap/ldap/v3"
 	"github.com/vultr/govultr/v2"
 	"golang.org/x/oauth2"
-	"github.com/go-ldap/ldap/v3"
+	"github.com/rs/cors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/rs/cors"
-	"github.com/gorilla/mux"
 )
-
-type RequestData struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Image    string `json:"image"`
-}
 
 var (
-	apiKey       = os.Getenv("VULTR_API_KEY")
-	vc           *govultr.Client
-	ctx          = context.Background()
-	s3Vultr      *s3.S3
-	bucketName   = "ewnix-avatars"
-	directory    = "users"
-	filename     = "avatar.png"
-	accessKey    = os.Getenv("ACCESS_KEY")
-	secretKey    = os.Getenv("SECRET_KEY")
-	sessionToken = os.Getenv("SESSION_TOKEN")
 	ldapServer   = os.Getenv("LDAP_SERVER")
 	ldapPort     = os.Getenv("LDAP_PORT")
-	ldapBaseUserDN = os.Getenv("LDAP_BASE_USER_DN")
+	ldapBaseDN   = os.Getenv("LDAP_BASE_USER_DN")
+	vultrAPIKey  = os.Getenv("VULTR_API_KEY")
+	bucketName   = "ewnix-avatars"
+	avatarSuffix = "avatar.png"
+	s3Vultr *s3.S3
 )
 
-func init() {
-    config := &oauth2.Config{}
-    ts := config.TokenSource(ctx, &oauth2.Token{AccessToken: apiKey})
-    vc = govultr.NewClient(oauth2.NewClient(ctx, ts))
+type UserRequest struct {
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Base64Image string `json:"image"`
 }
 
-func authenticateLDAP(username, password string) bool {
+func convertToPNG(imageData []byte) ([]byte, error) {
+	imgFormat := http.DetectContentType(imageData)
+
+	if imgFormat == "image/png" {
+		// Image is already in PNG format, no conversion needed
+		return imageData, nil
+	} else if imgFormat == "image/jpeg" || imgFormat == "image/jpg" {
+		img, _, err := image.Decode(bytes.NewReader(imageData))
+		if err != nil {
+			return nil, fmt.Errorf("JPEG decoding failed: %v", err)
+		}
+
+		var pngBuf bytes.Buffer
+		err = png.Encode(&pngBuf, img)
+		if err != nil {
+			return nil, fmt.Errorf("PNG encoding failed: %v", err)
+		}
+
+		return pngBuf.Bytes(), nil
+	} else {
+		return nil, fmt.Errorf("Unsupported image format: %s", imgFormat)
+	}
+}
+
+func uploadImageToStorage(username string, imageData []byte) error {
+	config := &oauth2.Config{}
+	ts := config.TokenSource(context.Background(), &oauth2.Token{AccessToken: vultrAPIKey})
+	vc := govultr.NewClient(oauth2.NewClient(context.Background(), ts))
+
+	objectKey := username + "/" + avatarSuffix
+
+	// Upload the image using s3Vultr.PutObject
+	_, err := s3Vultr.PutObject(&s3.PutObjectInput{
+		Body:   bytes.NewReader(imageData),
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+		// Set any other required parameters (ACL, ContentType, etc.)
+	})
+	if err != nil {
+		return fmt.Errorf("Image upload failed: %v", err)
+	}
+
+	log.Printf("Image for user %q uploaded successfully!", username)
+	return nil
+}
+
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	var userReq UserRequest
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&userReq); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
 	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%s", ldapServer, ldapPort))
 	if err != nil {
-		return false
+		http.Error(w, "LDAP connection error", http.StatusInternalServerError)
+		return
 	}
 	defer l.Close()
 
-	err = l.Bind(fmt.Sprintf("cn=%s", username)+","+ldapBaseUserDN, password)
-	return err == nil
-}
-
-
-func convertToPNG(jpegData []byte) ([]byte, error) {
-	cmd := exec.Command("convert", "-", "-format", "png", "-")
-	cmd.Stdin = bytes.NewReader(jpegData)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("Error converting JPEG to PNG: %v", err)
-	}
-	return out.Bytes(), nil
-}
-
-func uploadObject(data []byte, username string) error {
-	input := &s3.PutObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(fmt.Sprintf("%s/%s/%s", directory, username, filename)),
-		Body:   bytes.NewReader(data),
-	}
-	_, err := s3Vultr.PutObject(input)
-	return err
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// Read request
-	body, err := ioutil.ReadAll(r.Body)
+	userDN := fmt.Sprintf("cn=%s,%s", userReq.Username, ldapBaseDN)
+	err = l.Bind(userDN, userReq.Password)
 	if err != nil {
-		http.Error(w, "Failed to read request", http.StatusBadRequest)
+		http.Error(w, "LDAP authentication failed", http.StatusUnauthorized)
 		return
 	}
-	defer r.Body.Close()
 
-	// Decode JSON
-	var requestData RequestData
-	err = json.Unmarshal(body, &requestData)
+	imageData, err := base64.StdEncoding.DecodeString(userReq.Base64Image)
 	if err != nil {
-		http.Error(w, "Failed to decode JSON", http.StatusBadRequest)
+		http.Error(w, "Image decoding failed", http.StatusBadRequest)
 		return
 	}
 
-	// LDAP authentication
-	if !authenticateLDAP(requestData.Username, requestData.Password) {
-		http.Error(w, "Authentication failed", http.StatusUnauthorized)
-		return
-	}
-
-	// Decode the base64 image
-	decodedImage, err := base64.StdEncoding.DecodeString(requestData.Image)
+	pngData, err := convertToPNG(imageData)
 	if err != nil {
-		http.Error(w, "Error decoding image", http.StatusInternalServerError)
+		http.Error(w, "Image conversion failed", http.StatusInternalServerError)
 		return
 	}
 
-	// Convert JPEG to PNG if necessary
-	imageFormat := http.DetectContentType(decodedImage)
-	var finalImageData []byte
-	if imageFormat == "image/jpeg" || imageFormat == "image/jpg" {
-		pngData, err := convertToPNG(decodedImage)
-		if err != nil {
-			http.Error(w, "Error converting image to PNG", http.StatusInternalServerError)
-			return
-		}
-		finalImageData = pngData
-	} else {
-		finalImageData = decodedImage
-	}
-
-	// Upload the final image data
-	err = uploadObject(finalImageData, requestData.Username)
+	err = uploadImageToStorage(userReq.Username, pngData)
 	if err != nil {
-		http.Error(w, "Failed to upload image", http.StatusInternalServerError)
+		http.Error(w, "Image upload failed", http.StatusInternalServerError)
 		return
 	}
 
-	w.Write([]byte("Avatar uploaded!"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Image uploaded successfully"))
 }
-
 
 func main() {
-	r := mux.NewRouter()
-
-	// CORS setup
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://www.ewnix.net"},
-		AllowCredentials: true,
-		AllowedMethods:   []string{"POST"},
+	// Set up CORS middleware
+	corsOptions := cors.New(cors.Options{
+		AllowedOrigins: []string{"https://www.ewnix.net"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
 	})
 
-	r.HandleFunc("/upload", handleRequest)
+	// Create a mux with CORS middleware
+	mux := http.NewServeMux()
+	mux.HandleFunc("/upload", handleUpload)
 
-	// Apply CORS middleware
-	handler := c.Handler(r)
+	// Use CORS middleware with your handler
+	handler := corsOptions.Handler(mux)
 
-	http.Handle("/", handler)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
--- go.mod
+++ go.mod
@@ -4,17 +4,14 @@ go 1.19
 
 require (
 	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
-	github.com/aws/aws-sdk-go v1.44.322 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/aws/aws-sdk-go v1.44.323 // indirect
 	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
 	github.com/go-ldap/ldap/v3 v3.4.5 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/rs/cors v1.9.0 // indirect
 	github.com/vultr/govultr/v2 v2.17.2 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
